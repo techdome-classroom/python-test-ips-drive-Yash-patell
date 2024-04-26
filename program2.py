5328f7a7096e40ef518ab68f0cda9000fd97412c
Yash-patell <125695420+Yash-patell@users.noreply.github.com>
2024-04-26 13:16:59 +0530

    All Completed

--- program2.py
+++ program2.py
@@ -1,7 +1,7 @@
 def longest_substring(s: str) -> int:
     
     """"
-    
+
     Implement the function longest_substring 
     using the provided longest_substring function to find 
     the length of the longest substring without repeating characters.
