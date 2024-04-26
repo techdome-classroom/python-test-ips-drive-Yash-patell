0c7bee879cb00bc5ecdf152d267ea7e964699aaa
Yash-patell <125695420+Yash-patell@users.noreply.github.com>
2024-04-26 13:10:43 +0530

    Updated program2.py

--- program2.py
+++ program2.py
@@ -7,7 +7,22 @@ def longest_substring(s: str) -> int:
     the length of the longest substring without repeating characters.
 
     """ 
-    pass
+
+    char_index = {}
+    max_length = 0
+    start = 0
+
+    for end in range(len(s)):
+      
+        if s[end] in char_index and char_index[s[end]] >= start:
+            start = char_index[s[end]] + 1
+        char_index[s[end]] = end
+        max_length = max(max_length, end - start + 1)
+
+    return max_length
+
+
+  
 
 
 
