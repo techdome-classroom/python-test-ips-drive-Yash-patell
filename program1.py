4da7e9a2fc4588ade2cbe1ac02d66bfc0e164c1f
Yash-patell <125695420+Yash-patell@users.noreply.github.com>
2024-04-26 13:11:12 +0530

    Updated program1.py

--- program1.py
+++ program1.py
@@ -1,11 +1,27 @@
+from typing import List
 def smallest_missing_positive_integer(nums: List[int]) -> int:
+    n=len(nums)
     """
     Implement the function smallest_missing_positive_integer 
     using the provided smallest_missing_positive_integer function 
     to find the smallest missing positive integer in the given list.
 
     """
-    pass
+
+    
+    i = 0
+    while i < n:
+        if 0 < nums[i] <= n and nums[i] != nums[nums[i] - 1]:
+            nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        else:
+            i += 1
+    
+    for i in range(n):
+        if nums[i] != i + 1:
+            return i + 1
+    
+   
+    return n + 1
 
 
 
